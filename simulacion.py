2714dd65db9315773a3cb83f698dc8631e86898e
Oier Beaskoetxea <obeaskoetxea@gmail.com>
2021-12-02 13:14:32 +0100

    Update simulacion.py

--- simulacion.py
+++ simulacion.py
@@ -4,7 +4,6 @@ from utils import *
 from constantes import *
 from tecnico import Tecnico
 from servicio import Servicio
-import queue
 
 #class simulacion
 class Simulacion:
@@ -17,19 +16,17 @@ class Simulacion:
         self.tecnico = Tecnico()
         self.t_max = t_max
         self.t = 0
-        self.servicios_pendientes = queue.Queue()
         self.ejercicio = ejercicio
 
     #simular
     def simular(self):
         while (self.t < self.t_max):
-            if (self.servicios_pendientes.qsize() > 0): # siempre deberia haber al menos 1 servicio, pero lo comprobamos
-                servicio = self.servicios_pendientes.get() # obtenemos el primer servicio de la cola
-                self.tecnico.setServicio(servicio)
-                
-                t_desplazamiento = self.tecnico.iniciaDesplazamiento(servicio.getMunicipio()) # calculamos lo que tardaremos en desplazarnos
-                t_servicio = self.tecnico.iniciaServicio() # calculamos lo que tardaremos en hacer el servicio
-                self.t += t_desplazamiento + t_servicio # actualizamos el tiempo
+            servicio = self.generarServicio() # obtenemos el primer servicio de la cola
+            self.tecnico.setServicio(servicio)
+            
+            t_desplazamiento = self.tecnico.iniciaDesplazamiento(servicio.getMunicipio()) # calculamos lo que tardaremos en desplazarnos
+            t_servicio = self.tecnico.iniciaServicio() # calculamos lo que tardaremos en hacer el servicio
+            self.t += t_desplazamiento + t_servicio # actualizamos el tiempo
     
     def generarServicio(self):
         # NOTE: no se cuando se genera un servicio pero se haria de esta forma
@@ -40,10 +37,10 @@ class Simulacion:
             gravedad = getGravedadRandom()
             servicio.setGravedad(gravedad)
 
-        self.servicios_pendientes.put(servicio)
+        return servicio
 
 
 if __name__ == '__main__':
     simul = Simulacion()
     simul.simular()
-
+    
