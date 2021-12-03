ba6488009a186c76d61778fa5d8b277493772e3a
MiquelEspinosa <mespinosami@gmail.com>
2021-12-03 11:02:53 +0100

    markov chains tiempo continuo acabado

--- constantes.py
+++ constantes.py
@@ -1,22 +1,20 @@
 # constantes para facilitar calculos despues
 MAX_H_MES = 160
-MEDIA_H_SERVICIO = 2 # horas de media para hacer un servicio
+# MEDIA_H_SERVICIO = 2 # horas de media para hacer un servicio
 
 T_MAX = MAX_H_MES * 60 # el tiempo maximo se mide en minutos
-MEDIA_MINS_SERVICIO = MEDIA_H_SERVICIO * 60 # minutos de media para hacer un servicio
+# MEDIA_MINS_SERVICIO = MEDIA_H_SERVICIO * 60 # minutos de media para hacer un servicio
 
 # constantes que usaremos en el sistema
-MUNICIPIO_ARAN = 0
-MUNICIPIO_OCA = 1
-MEDIA_MINS_DESP = (15, 10) # minutos de media de desplazamiento segun el municipio
-MEDIA_MINS_DESP_MUNICIPOS = 30 # minutos de media de desplazamiento entre municipios
+# MUNICIPIO_ARAN = 0
+# MUNICIPIO_OCA = 1
+# MEDIA_MINS_DESP = (15, 10) # minutos de media de desplazamiento segun el municipio
+# MEDIA_MINS_DESP_MUNICIPOS = 30 # minutos de media de desplazamiento entre municipios
 
-TEC_DESPLAZANDOSE = 0
-TEC_EN_SERVICIO = 1
 
 # apartado B del ejercicio
-EJERCICIO_A = 0
-EJERCICIO_B = 1
+EJERCICIO_A = "A"
+EJERCICIO_B = "B"
 
 SERVICIO_LEVE = 0
 SERVICIO_ESTANDAR = 1
@@ -24,4 +22,27 @@ SERVICIO_GRAVE = 2
 PROB_SERVICIOS = (35, 45, 20)
 MEDIA_MINS_SERVICIO_CAT = (15, 30, 55)
 VARIANZA_SERVICIO_CAT = (4, 6, 9)
+
+
+
+TIEMPO_ESTADO = [120, 120, 15, 10, 30, 30]
+
+
+TABLA_ALIAS = [
+        [0.00, 0.00, 0.00, 0.25, 0.75, 0.00],
+        [0.00, 0.00, 0.75, 0.00, 0.00, 0.25],
+        [0.00, 1.00, 0.00, 0.00, 0.00, 0.00],
+        [1.00, 0.00, 0.00, 0.00, 0.00, 0.00],
+        [0.00, 1.00, 0.00, 0.00, 0.00, 0.00],
+        [1.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+]
+
+ESTADOS = {
+    0: "ServicioOcaña",
+    1: "ServicioAranjuez",
+    2: "ViajeAA",
+    3: "ViajeOO",
+    4: "ViajeOA",
+    5: "ViajeAO",
+}
 
---
+++ main.py
@@ -0,0 +1,80 @@
+from constantes import *
+import numpy as np
+import random
+
+
+def estado_inicial():
+    return random.randint(2,5) # estado inicial: un estado de desplazamiento
+
+
+def estado_siguiente(estado):
+    r = random.random()
+    accum = 0
+    for index, prob in enumerate(TABLA_ALIAS[estado]):
+        accum = accum+prob
+        if r < accum:
+            return index
+
+def getGravedadRandom():
+    x = random.randint(0, 100) # para sacar un porcentaje
+    fin = False
+    prob = 0
+    pos = 0
+    while (not fin and pos < len(PROB_SERVICIOS)):
+        prob += PROB_SERVICIOS[pos]
+        if (x <= prob):
+            fin = True
+        else:
+            pos += 1
+    
+    return pos
+
+
+def simular(ejercicio):
+    t = 0
+    estado = estado_inicial()
+    tiempo_desplazamientos = 0
+    servicios_totales = 0
+    
+    tiempos_aranjuez_ocaña = []
+    tiempo_accumulado_a_o = 0
+    contador_tiempo = False
+
+
+    while (t < T_MAX):
+
+        if ejercicio == EJERCICIO_A or estado > 1:
+            t_servicio = np.random.exponential(TIEMPO_ESTADO[estado])
+
+        elif ejercicio == EJERCICIO_B:
+            gravedad = getGravedadRandom()
+            m_servicio = MEDIA_MINS_SERVICIO_CAT[gravedad]
+            v_servicio = VARIANZA_SERVICIO_CAT[gravedad]
+            t_servicio = random.normalvariate(m_servicio, v_servicio)
+
+        if estado > 1: # estado es de desplazamiento
+            tiempo_desplazamientos += t_servicio
+        else: # estado es de servicio
+            servicios_totales += 1
+
+        t = t + t_servicio
+
+        if contador_tiempo == True:
+            tiempo_accumulado_a_o += t_servicio
+
+        if estado == 1 and contador_tiempo == False:
+            contador_tiempo = True
+
+        if estado == 0 and contador_tiempo == True:
+            tiempos_aranjuez_ocaña.append(tiempo_accumulado_a_o)
+            tiempo_accumulado_a_o = 0
+            contador_tiempo = False
+
+        estado = estado_siguiente(estado)
+
+    print("Tiempo total de ejecución: ",t," minutos")
+    print("Tiempo empleado en desplazamientos: ",tiempo_desplazamientos)
+    print("Número medio de servicios al mes: ",servicios_totales)
+    print("Tiempo medio aranjuez - ocaña: ",sum(tiempos_aranjuez_ocaña)/len(tiempos_aranjuez_ocaña))
+
+simular("B")
